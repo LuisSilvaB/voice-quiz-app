3dc1ddf4aa83cb95de6932696587dcb3b220b798
LuisSilvaB <LuisSilvaBalladares@hotmail.com>
2024-02-14 21:41:28 -0500

    feat(routes): add new routes

--- voice-quiz-front/src/routes/routes.tsx
+++ voice-quiz-front/src/routes/routes.tsx
@@ -1,9 +1,11 @@
 import { useRoutes } from "react-router-dom";
 import Home from "../pages/home/home";
+import Login from "../pages/auth/login/index";
 
 const RoutesContainer = () => {
     const routes = useRoutes([
         { path: "/", element: <Home /> },
+        { path: "/login", element: <Login /> },
     ])
 
     return routes;
