742b52421c40b3bbc3a30cd9b451626c6e60bc7a
LuisSilvaB <LuisSilvaBalladares@hotmail.com>
2024-03-24 20:28:59 -0500

    feat(types): add new atributes to fragment shape

--- voice-quiz-front/src/interface/types.ts
+++ voice-quiz-front/src/interface/types.ts
@@ -1,5 +1,11 @@
 export type Status = "idle" | "recording" | "transcribing" | "streaming";
 export type ModalActions = "edit" | "delete" | "create" | null; 
+export interface toggleProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  onToggle: () => void;
+}
 
 export interface OpenAIMessage {
   content: string;
@@ -7,7 +13,7 @@ export interface OpenAIMessage {
   name?: string;
   function_call?: {
     name: string;
-      arguments: string;
+    arguments: string;
     };
   }
   export interface tableSessionsShape {
@@ -27,9 +33,18 @@ export interface CourseShape {
   placeholderImg: string;
   sessions: Session[];
 }
-export interface toggleProps {
-  isOpen: boolean;
-  onOpen: () => void;
-  onClose: () => void;
-  onToggle: () => void;
+export interface fragmentShape {
+  id:string, 
+  content: string;
+  title?: string | undefined;
+  questions?: question[];
 }
+export interface question {
+  id:string; 
+  questionTitle:string;
+  alternatives: string[];
+  answer:string;
+  kindQuestion: kindQuestion;
+}
+
+export type kindQuestion = "multiple answers" | "alternatives" | "question and answer"
